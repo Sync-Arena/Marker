b236add717cef71355311e823bae87f349767f7c
Mahmoud-Hawara <mahmoudhawara115@gmail.com>
2024-01-06 23:35:13 +0200

    enhance the design

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Input from './Components/input/Input';
 import Output from './Components/output/Output';
+import Header from './Components/header/Header';
 import './App.css';
 
 function App() {
@@ -13,8 +14,12 @@ function App() {
 
   return (
     <div className="App">
-        <Input setContent={(e) => handleContentChange(e)}/>
-        <Output content={inputContent}/>
+        <Header />
+        <div className='container'>
+          <Input setContent={(e) => handleContentChange(e)}/>
+          <div style={{ borderRight: '2px solid #FFA116'}}></div>
+          <Output content={inputContent}/>
+        </div>
     </div>
   );
 }
---
+++ src/Components/header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className='label'>
+            <p className='marker'>MARKER</p>
+            <p className='org'>sync arena</p>
+        </div>
+    </div>
+  )
+}
+
+export default Header
