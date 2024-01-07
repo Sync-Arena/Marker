ea652a8fe467655c97195909b59fe8b14d58140d
Mahmoud-Hawara <mahmoudhawara115@gmail.com>
2024-01-07 18:45:53 +0200

    fix the code-area bug

--- src/Components/code/Code.js
+++ src/Components/code/Code.js
@@ -1,31 +1,43 @@
 import React, { useEffect, useState } from 'react';
-import SyntaxHighlighter from 'react-syntax-highlighter';
 import { materialDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { CopyIcon, Copied } from '../icons';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import './Code.css';
 
-const Code = ({ children, language }) => {
+const Code = (props) => {
   const [isCopied, setIsCopied] = useState(false);
 
   useEffect(() => {
     const timeoutId = setTimeout(() => {
       setIsCopied(false);
-    }, 2000);
+    }, 1500);
 
     return () => clearTimeout(timeoutId);
   }, [isCopied]);
 
   return (
     <div className='code'>
-      <CopyToClipboard text={children} onCopy={() => setIsCopied(true)}>
+      <CopyToClipboard text={props.children} onCopy={() => setIsCopied(true)}>
         <button className='icon'>
-          {isCopied ? <Copied /> : <CopyIcon className='copy-icon' />}
+          {isCopied ? <Copied /> : <CopyIcon />}
         </button>
       </CopyToClipboard>
-      <SyntaxHighlighter language={language} style={materialDark}>
-        {children}
-      </SyntaxHighlighter>
+      {props.known === 1 
+        ? 
+        <SyntaxHighlighter
+          children={String(props.children).replace(/\n$/, '')}
+          language={props.lang}
+          style={dark}
+        />
+        : 
+        <pre>
+          <code>
+            {props.children}
+          </code>
+        </pre>
+      }
     </div>
   );
 };
--- src/Components/output/Output.js
+++ src/Components/output/Output.js
@@ -1,23 +1,48 @@
 import React from 'react';
-import Markdown from 'markdown-to-jsx';
-import Code from '../code/Code';
+import Code from '../code/Code.js';
+import Markdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
+import 'katex/dist/katex.min.css';
 import './Output.css';
 
 const Output = (props) => {
   return (
     <div className='output-container'>
-      <Markdown 
-        className='output' 
-        options={{
-          overrides: {
-            code: {
-              component: Code,
-            },
+      <Markdown
+        className='output'
+        remarkPlugins={[remarkMath, remarkGfm]}
+        rehypePlugins={[rehypeKatex]}
+        children={props.content}
+        components={{
+          pre(props) {
+            const { children } = props;
+            let codeContent;
+            let lang;
+            const hasSpecificLang = React.Children.toArray(children).some(
+              (child) => {
+                if (
+                  React.isValidElement(child) &&
+                  child.type === 'code'
+                ) {
+                  codeContent = child.props.children;
+                  lang = /language-(\w+)/.exec(child.props.className || '');
+                  if (child.props.className) {
+                    return child.props.className !== "";
+                  }
+                }
+                return false;
+              }
+            );
+            return hasSpecificLang ? (
+              <Code known={1} children={codeContent} lang={lang[1]} />
+            ) : (
+              <Code known={0} children={codeContent} />
+            );
           },
         }}
-      >
-        {props.content}
-      </Markdown>
+      />
     </div>
   );
 };
